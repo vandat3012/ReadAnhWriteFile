7685b8e18e549700e92911165638731074b23adf
vandat3012 <luongvandat301220@gmail.com>
2023-12-28 15:25:50 +0700

    update bai tap

---
+++ src/ReadAndWriteFile.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadAndWriteFile {
+    public List<Integer> readFile(String filePath) {
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                numbers.add(Integer.parseInt(line));
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            System.out.println("File không tồn tại hoặc có lỗi ");
+        }
+        return numbers;
+    }
+    public void writeFile (String filePath, int max){
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write("Giá trị lớn nhat là : " + max);
+            bufferedWriter.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
